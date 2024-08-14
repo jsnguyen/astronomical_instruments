40938aa80b5ab4f7b8c0124e3abb55cfe4acc62e
Jayke Nguyen <jsn@saved.local>
2024-08-13 21:37:25 -0700

    good first vers

--- README.md
+++ README.md
@@ -1,24 +1,6 @@
 # NIRC2 Non-Linearity Correction
 
-Upgrades to NIRC2 in late 2023 slightly changed the behavior of the detector, notably, the gains are different. Changing the gain has resultingly changed the linearity characteristics of the detector. We perform an analysis using reduced data from early 2024 with the new instrument upgrades and characterize the non-linearity in the system. A function to correct for the non-linearity is provided. However, recent changes (~July 2024) require that a linearity test be performed again.
-
-In 2024, linearity data was collected in Kp and Ks band. While we don't expect any difference between wavelengths, slight variaiont is observed in the dataset.
-
-Plots showing the non-linearity:
-
-<div>
-    <img src="plots/linearity_2019.png" alt="linearity 2019 plot" width="300"/>
-    <img src="plots/linearity_2024.png" alt="linearity 2024 plot" width="300"/>
-</div>
-
-Plots showing the percent deviation from linearity:
-
-<div>
-    <img src="plots/diff_linearity_2019.png" alt="diff 2019 plot" width="300"/>
-    <img src="plots/diff_linearity_2024.png" alt="diff 2024 plot" width="300"/>
-</div>
-
-The correction curves were derived from fits to the percent deviation from linearity.
+Upgrades to NIRC2 in late 2023 slightly changed the behavior of the detector, notably, the gains are different. Changing the gain has resultingly changed the linearity characteristics of the detector. We perform an analysis using reduced data from early 2024 with the new instrument upgrades and characterize the non-linearity in the system. A function to correct for the non-linearity is provided. However, recent changes (~July 2024) require that a linearity test be performed again and new data needs to be taken.
 
 ## Installation
 
@@ -41,6 +23,38 @@ If you want to regenerate the correction curves, and diagnostic plots:
 ``` python
 from nirc2_nonlinearity_correction import linearity
 
-linearity('2019')
 linearity('2024')
+linearity('2019')
+
+ # make the check plots
+linearity('2024', use_correction_curve=True, save_curves=False)
+linearity('2019', use_correction_curve=True, save_curves=False)
 ```
+
+## Analysis
+
+In 2024, linearity data was collected in Kp and Ks band. Data was collected simply by turning on the flat lamps and exposing for increasing amounts of time. While we don't expect any difference between wavelengths, slight variation is observed in the dataset. Both 2019 and 2024 datasets are included in this repository.
+
+Plots showing the non-linearity:
+
+<div>
+    <img src="plots/linearity_2019.png" alt="linearity 2019 plot" width="300"/>
+    <img src="plots/linearity_2024.png" alt="linearity 2024 plot" width="300"/>
+</div>
+
+Plots showing the percent deviation from linearity:
+
+<div>
+    <img src="plots/linearity_diff_2019.png" alt="diff 2019 plot" width="300"/>
+    <img src="plots/linearity_diff_2024.png" alt="diff 2024 plot" width="300"/>
+</div>
+
+The correction curves were derived from polynomial fits to the percent deviation from linearity. Note that we cut off some points on each end of the curve to fit 100 to 20000 DN in 2019, 100 to 5000 DN in 2024 a little better. In practice, data values on images typically fall somewhere in this range.
+
+Lastly, checking that our correction works:
+<div>
+    <img src="plots/CHECK_linearity_2019.png" alt="check linearity 2019 plot" width="300"/>
+    <img src="plots/CHECK_linearity_2024.png" alt="check linearity 2024 plot" width="300"/>
+</div>
+
+And we see that the plots are now much closer to linear across the range! The upper ends aren't so important to fit since the detector typically does not reach those levels.
--- src/nirc2_nonlinearity_correction/linearity.py
+++ src/nirc2_nonlinearity_correction/linearity.py
@@ -13,7 +13,9 @@ from astropy.io.fits.verify import AstropyUserWarning
 import warnings
 warnings.simplefilter('ignore', category=AstropyUserWarning)
 
-def plot_linearity(linearity_data, n_fit, bandname, year):
+from correction_curve import NIRC2
+
+def plot_linearity(linearity_data, n_fit, year, use_correction_curve):
 
     fig, ax = plt.subplots(figsize=(6,6))
     for bandname in linearity_data.keys():
@@ -24,7 +26,11 @@ def plot_linearity(linearity_data, n_fit, bandname, year):
         ax.plot(exp_times[:n_fit], linearity_line(exp_times[:n_fit]), linestyle='--', color='black', alpha=0.7)
         ax.plot(exp_times[n_fit-1:], linearity_line(exp_times[n_fit-1:]), linestyle='--', color='black', alpha=0.3)
 
-        ax.set_title(f'Linearity {year}')
+        if use_correction_curve:
+            ax.set_title(f'CHECK Linearity {year}')
+        else:
+            ax.set_title(f'Linearity {year}')
+
         ax.set_xlabel('Exposure Time [s]')
         ax.set_ylabel('Average Pixel Count [DN/px]')
         
@@ -34,10 +40,12 @@ def plot_linearity(linearity_data, n_fit, bandname, year):
     ax.grid()
     ax.legend()   
 
-    plt.savefig(f'linearity_{year}.png', bbox_inches='tight')
-    plt.savefig(f'linearity_{year}.pdf', bbox_inches='tight')
+    if use_correction_curve:
+        plt.savefig(f'CHECK_linearity_{year}.png', bbox_inches='tight')
+    else:
+        plt.savefig(f'linearity_{year}.png', bbox_inches='tight')
 
-def plot_linearity_deviation(linearity_data, correction_curves, diffs, cutoff, bandname, year):
+def plot_linearity_deviation(linearity_data, correction_curves, diffs, cutoff, year, use_correction_curve):
     
     means = []
     fig, ax = plt.subplots(figsize=(6,6))
@@ -54,7 +62,10 @@ def plot_linearity_deviation(linearity_data, correction_curves, diffs, cutoff, b
         ax.errorbar(full_data_mean[cutoff:], diff[cutoff:]*100, yerr=np.abs(weights[cutoff:]*diff[cutoff:]*100), marker='o', alpha=0.7, label=f'Percent Deviation {bandname.capitalize()}-Band')
         ax.plot(fit_xs, np.vectorize(nonlinearity_fit)(fit_xs)*100, linestyle='-', color=ax.get_lines()[-1].get_c(), label='2nd Order Polynomial Fit')
 
-    ax.set_title(f'Deviation From Linearity {year}')
+        if use_correction_curve:
+            ax.set_title(f'CHECK Deviation From Linearity {year}')
+        else:
+            ax.set_title(f'Deviation From Linearity {year}')
 
     ax.set_xlabel('Average Pixel Count [DN/px]')
     ax.set_ylabel('Percent Deviation From Linearity [%]')
@@ -65,15 +76,12 @@ def plot_linearity_deviation(linearity_data, correction_curves, diffs, cutoff, b
     ax.grid()
     ax.legend()
 
-    plt.savefig(f'diff_linearity_{year}.png', bbox_inches='tight')
-    plt.savefig(f'diff_linearity_{year}.pdf', bbox_inches='tight')
+    if use_correction_curve:
+        plt.savefig(f'CHECK_linearity_diff_{year}.png', bbox_inches='tight')
+    else:
+        plt.savefig(f'linearity_diff_{year}.png', bbox_inches='tight')
 
-def fit_linearity(data_files, cutoff, n_fit, order=1):
-    data = []
-    headers = []
-    for fn in data_files:
-        data.append(fits.getdata(fn)/4) # divide by 4 to get back into units of DN
-        headers.append(fits.getheader(fn))
+def fit_linearity(data, headers, cutoff, n_fit, order=1):
     
     data_file = importlib.resources.files('nirc2_nonlinearity_correction.data').joinpath(f'nirc2mask.fits')
     bad_pixel_mask = fits.getdata(data_file)
@@ -96,55 +104,68 @@ def fit_linearity(data_files, cutoff, n_fit, order=1):
 
     # fitting a line to the first n points
     linearity_line = Polynomial.fit(exp_times[:n_fit], full_data_mean[:n_fit], order, w=weights[:n_fit])
-    print(linearity_line.convert().coef)
 
     return exp_times, linearity_line, full_data_mean, full_data_std
 
-def make_nonlinearity_function(exp_times, linearity_line, full_data_mean, full_data_std, correction_curve_slice, order=1):
+def calc_correction_curves(linearity_data, correction_curve_slice, order=1):
+
     dn, up = correction_curve_slice
-
-    expected = linearity_line(exp_times)
-    diff = (full_data_mean - expected) /  expected
-    weights = full_data_std/full_data_mean # higher number = more weight
-
-    nonlinearity_fit = Polynomial.fit(full_data_mean[dn:up], diff[dn:up], order, w=weights[dn:up])
-    return nonlinearity_fit, diff
-
-def calc_correction_curves(linearity_data, correction_curve_slice):
 
     diffs = {}
     correction_curves = {}
     for bandname in linearity_data.keys():
         exp_times, linearity_line, full_data_mean, full_data_std = linearity_data[bandname]
 
-        nonlinearity_fit, d = make_nonlinearity_function(exp_times, linearity_line, full_data_mean, full_data_std, correction_curve_slice, order=1)
+        expected = linearity_line(exp_times)
+        diff = (full_data_mean - expected) /  expected
+        weights = full_data_std/full_data_mean # higher number = more weight
+
+        nonlinearity_fit = Polynomial.fit(full_data_mean[dn:up], diff[dn:up], order, w=weights[dn:up])
+        print(f'{bandname.capitalize()}-Band Fit parameters: {nonlinearity_fit.convert().coef}')
+
         correction_curves[bandname] = nonlinearity_fit
-        diffs[bandname] = d
+        diffs[bandname] = diff
 
     return correction_curves, diffs
 
-def linearity(year: str):
+def linearity(year, cutoff=None, correction_curve_slice=None, use_correction_curve=False, save_curves=True, order=1):
+
+    '''
+    read in the datasets, fit the linearity, make plots, and save the final correction curves
+
+    args:
+        year: can be '2019' or '2024' only, the two data sets that are available
+        cutoff: the lower end cutoff for the fits
+        correction_curve_slice: slices the corrected diff data to chop off the beginning or end to do a fit on the linear part of the system
+        use_correction_curve: uses the pre-computed correction curve to check if it worked correctly
+        save_curves: save .npy files
+        order: the order of the final fit for the correction curve
+
+    return:
+        None
+    '''
 
     valid_years = ['2019', '2024']
     if year not in valid_years:
         print(f'Year must be in {valid_years}!')
         return
 
+    # cutoffs are needed in the fit to fit a curve better to the linear region in the detector
+
     if year == '2024':
         bands = ['kp', 'ks'] # h band seems to be bad for this data
         cutoff = 1 # cutoff of the beginning few elements that are bad
-        correction_curve_slice = 3,-1
+        correction_curve_slice = 3,-1 # cutoff the first few, and the last one to get a better fit
         n_fit = 5 # fit to the first n elements
 
     elif year == '2019':
         bands = ['h']
         cutoff = 0 # cutoff of the beginning few elements that are bad
-        correction_curve_slice = 3,-2
+        correction_curve_slice = 3,-2 # cutoff the first few and last one to get a better fit
         n_fit = 6 # fit to the first n elements
 
     linearity_data = {}
     for bandname in bands:
-        linearity_data[bandname] = []
 
         data_folder = importlib.resources.files(f'nirc2_nonlinearity_correction.data_{year}.linearity_{bandname}')
         data_files = []
@@ -153,18 +174,38 @@ def linearity(year: str):
                 data_files.append(resource)
         data_files = sorted(data_files)
 
-        res = fit_linearity(data_files, cutoff, n_fit)
+        data = []
+        headers = []
+        for fn in data_files:
+            d = fits.getdata(fn)/4 # divide by 4 to get back into units of DN
+            if use_correction_curve:
+                d = NIRC2.apply_nonlinearity_correction(d, year)
+
+            data.append(d) 
+            headers.append(fits.getheader(fn))
+
+
+        res = fit_linearity(data, headers, cutoff, n_fit)
         linearity_data[bandname] = res
 
-    correction_curves, diffs = calc_correction_curves(linearity_data, correction_curve_slice)
+    correction_curves, diffs = calc_correction_curves(linearity_data, correction_curve_slice, order=order)
 
-    filename = f'correction_curve_{year}.npy'
-    if len(correction_curves.keys()) > 1:
-        for key in correction_curves.keys():
-            correction_curves[key] = correction_curves[key].convert(domain=[0, 10000])
-        np.save(filename, np.mean(list(correction_curves.values())).convert().coef)
-    else:
-        np.save(filename, list(correction_curves.values())[0].convert().coef)
+    if save_curves:
+        filename = f'correction_curve_{year}.npy'
+        if len(correction_curves.keys()) > 1:
+            for key in correction_curves.keys():
+                correction_curves[key] = correction_curves[key].convert(domain=[0, 10000])
+            np.save(filename, np.mean(list(correction_curves.values())).convert().coef)
+        else:
+            np.save(filename, list(correction_curves.values())[0].convert().coef)
 
-    plot_linearity(linearity_data, n_fit, bandname, year)
-    plot_linearity_deviation(linearity_data, correction_curves, diffs, cutoff, bandname, year)
+    plot_linearity(linearity_data, n_fit, year, use_correction_curve)
+    plot_linearity_deviation(linearity_data, correction_curves, diffs, cutoff, year, use_correction_curve)
+
+if __name__=='__main__':
+    linearity('2024')
+    linearity('2019')
+    linearity('2024', use_correction_curve=True, save_curves=False)
+    linearity('2019', use_correction_curve=True, save_curves=False)
+
+#linearity('2019')
